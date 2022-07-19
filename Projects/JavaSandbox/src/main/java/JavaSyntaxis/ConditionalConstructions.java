a5cb12b307ca5231bc9f8b0d4e0c50e04400c733
V.Shilaev <valentin.shilaev@yandex.ru>
2022-07-19 14:56:17 +0300

    change folder structure + add some conspects

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/ConditionalConstructions.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/ConditionalConstructions.java
@@ -1,2 +1,15 @@
-package JavaSyntaxis;public class ConditionalConstructions {
+package JavaSyntaxis;
+
+public class ConditionalConstructions {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+        if (isTrue) {
+            // do this
+        } else if (isFalse) {
+            // do this
+        } else {
+            // do this
+        }
+    }
 }
