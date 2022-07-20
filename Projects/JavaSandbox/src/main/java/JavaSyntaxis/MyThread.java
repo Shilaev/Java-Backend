94a7eda0ef2a9297cbe4af89a8814a5b37fa49f7
V.Shilaev <valentin.shilaev@yandex.ru>
2022-07-20 14:26:48 +0300

    add arrays and other stuf

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/MyThread.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/MyThread.java
@@ -1,2 +1,51 @@
-package JavaSyntaxis;public class Thread {
+package JavaSyntaxis;
+
+class MyThread extends Thread {
+
+    public static void main(String[] args) {
+        Thread myThread = new MyThread();
+        myThread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("hello world");
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (HelloWorld.class) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("this is thread");
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (HelloWorld.class) {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("This is thread1");
+                }
+            }
+        });
+
+        thread.start();
+        thread1.start();
+
+        thread.join();
+        System.out.println("finished");
+
+    }
 }
--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/While.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/While.java
@@ -1,2 +1,16 @@
-package JavaSyntaxis;public class While {
+package JavaSyntaxis;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i != 2) {
+            System.out.println("bla bla");
+        }
+
+        do {
+            System.out.println("bla bla");
+            i++;
+        } while (i != 2);
+    }
 }
