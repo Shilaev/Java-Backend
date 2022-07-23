95425fa3ae8de12871da44b6df2508a321818f66
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-07-23 10:30:32 +0300

    HashMap conspect

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/Map_Example.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/Map_Example.java
@@ -1,2 +1,23 @@
-package JavaSyntaxis.Collections;public class Map_Example {
+package JavaSyntaxis.Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_Example {
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <= 100; i++) map.put(i, i * 10);
+
+        map.put(1, 1000000);
+        map.get(1); // 1000000
+
+        for (int elem : map.values()) System.out.println(elem);
+        for(int elem: map.keySet()) System.out.println(elem);
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            entry.getKey();
+            entry.getValue();
+        }
+
+    }
 }
