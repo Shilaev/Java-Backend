0a4f3555db03ca62a42947e0078a4cf5779168ba
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-07-23 19:40:30 +0300

    hash explanation

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/HashExplanationExample.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/HashExplanationExample.java
@@ -1,2 +1,56 @@
-package JavaSyntaxis.Collections;public class HashExplanationExample {
+package JavaSyntaxis.Collections;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+
+public class HashExplanationExample {
+    public static void main(String[] args) {
+        Set<Person> personSet = new HashSet<>();
+
+        personSet.add(new Person(1, "Mike"));
+        personSet.add(new Person(1, "Mike"));
+
+        System.out.println(personSet);
+
+        //
+
+        Set<String> stringSet = new HashSet<>();
+
+        stringSet.add("MyString");
+        stringSet.add("MyString");
+
+        System.out.println(stringSet);
+    }
 }
