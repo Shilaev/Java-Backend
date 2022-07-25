2430678ba589cf9888fc2ca0bc52397888a0a053
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-07-25 16:39:58 +0300

    add queue explanation

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/QueueExample.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/Collections/QueueExample.java
@@ -1,2 +1,49 @@
-package JavaSyntaxis.Collections;public class QueueExample {
+package JavaSyntaxis.Collections;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Character> characterQueue = new LinkedList<>();
+
+        characterQueue.add(new Character("Vanya"));
+        characterQueue.add(new Character("Olya"));
+        characterQueue.add(new Character("Evgeniy"));
+        characterQueue.add(new Character("Valentin"));
+        characterQueue.add(new Character("Nastya"));
+
+        while (!characterQueue.isEmpty()){
+            System.out.println(characterQueue.poll().getName());
+        }
+
+        Queue<Character> characterArrayBlockingQueue = new ArrayBlockingQueue<>(5);
+
+        characterArrayBlockingQueue.add(new Character("Vanya"));
+        characterArrayBlockingQueue.add(new Character("Olya"));
+        characterArrayBlockingQueue.add(new Character("Evgeniy"));
+        characterArrayBlockingQueue.add(new Character("Valentin"));
+        characterArrayBlockingQueue.add(new Character("Nastya"));
+
+        while (!characterArrayBlockingQueue.isEmpty()){
+            System.out.println(characterArrayBlockingQueue.poll().getName());
+        }
+
+    }
 }
+
+class Character {
+    private String name;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
