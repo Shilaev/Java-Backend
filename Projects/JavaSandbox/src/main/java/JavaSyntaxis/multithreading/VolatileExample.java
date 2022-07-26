31a5ba4ddb5818996987268ac0275f4e458ded3b
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-07-26 15:53:38 +0300

    volatile example

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/VolatileExample.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/VolatileExample.java
@@ -1,2 +1,34 @@
-package JavaSyntaxis.multithreading;public class VolatileExample {
+package JavaSyntaxis.multithreading;
+
+import java.util.Scanner;
+
+class MyRunningThread extends Thread {
+    
+    private volatile boolean isRunning = true;
+    
+    @Override
+    public void run() {
+        while (isRunning) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { throw new RuntimeException(e); }
+        }
+    }
+    
+    public void shutDown() {
+        this.isRunning = false;
+    }
+}
+
+public class VolatileExample {
+    public static void main(String[] args) {
+        MyRunningThread myRunningThread = new MyRunningThread();
+        myRunningThread.start();
+    
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        
+        myRunningThread.shutDown();
+    }
 }
