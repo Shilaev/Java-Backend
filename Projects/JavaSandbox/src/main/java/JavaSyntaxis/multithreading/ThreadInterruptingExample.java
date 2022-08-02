85f9254bb91edf9de9152dada5245415aa19414c
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-02 18:36:00 +0300

    update interview questions

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/ThreadInterruptingExample.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/ThreadInterruptingExample.java
@@ -1,6 +1,9 @@
 package JavaSyntaxis.multithreading;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ThreadInterruptingExample {
     public static void main(String[] args) throws InterruptedException {
