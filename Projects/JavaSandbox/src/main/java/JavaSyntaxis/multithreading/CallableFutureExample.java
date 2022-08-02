8e1e1857060c5954e8ba726fb4b754202d21801d
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-02 19:04:12 +0300

    update CallableFutureExample

--- Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/CallableFutureExample.java
+++ Projects/JavaSandbox/src/main/java/JavaSyntaxis/multithreading/CallableFutureExample.java
@@ -1,20 +1,33 @@
 package JavaSyntaxis.multithreading;
 
 import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
-public class CallableExample {
+public class CallableFutureExample {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
-        executorService.submit(new Callable<Integer>() {
+        Future<Integer> future = executorService.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
                 Random random = new Random();
-                return random.nextInt(100);
+                
+                int randomNumber = random.nextInt(100);
+                if (randomNumber < 40) {
+                    throw new Exception("less then 40");
+                }
+                
+                return randomNumber;
             }
         });
+        executorService.shutdown();
         
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            Throwable ex = e.getCause();
+            System.out.println(ex.getMessage());
+        }
     }
 }
