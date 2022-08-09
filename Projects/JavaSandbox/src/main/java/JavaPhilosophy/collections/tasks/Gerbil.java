519ea5e77cb4484126c5cfef7100eafc0de3d7b8
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-09 20:09:47 +0300

    update example, update conspect

--- Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/tasks/Gerbil.java
+++ Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/tasks/Gerbil.java
@@ -1,2 +1,28 @@
-package JavaPhilosophy.collections.tasks;public class Gerbil {
+package JavaPhilosophy.collections.tasks;
+
+import java.util.ArrayList;
+
+public class Gerbil {
+    int gerbilNumber;
+
+    public Gerbil(int gerbilNumber) {
+        this.gerbilNumber = gerbilNumber;
+    }
+
+    public String hop(){
+        String result = gerbilNumber + " and short message";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Gerbil> gerbils = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            gerbils.add(new Gerbil(i));
+        }
+
+        for (int i = 0; i < gerbils.size(); i++) {
+            System.out.println(gerbils.get(i).hop());
+        }
+    }
+
 }
