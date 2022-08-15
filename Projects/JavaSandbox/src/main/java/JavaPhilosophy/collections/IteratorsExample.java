bd65d3821ee7316e5bb9f8232e49cdd2fe0a3dc1
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-15 12:21:46 +0300

    update conspect

--- Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/IteratorsExample.java
+++ Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/IteratorsExample.java
@@ -1,2 +1,34 @@
-package JavaPhilosophy.collections;public class Iterators {
+package JavaPhilosophy.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class PersonForIteratorsExample {
+    private int id;
+
+    public PersonForIteratorsExample(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "" + id;
+    }
+}
+
+public class IteratorsExample {
+
+    public static void main(String[] args) {
+        int size = 25;
+        List<PersonForIteratorsExample> persons = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            persons.add(new PersonForIteratorsExample(i));
+
+        Iterator<PersonForIteratorsExample> personIterator = persons.iterator();
+
+        while (personIterator.hasNext()){
+            System.out.println(personIterator.next());
+        }
+    }
 }
