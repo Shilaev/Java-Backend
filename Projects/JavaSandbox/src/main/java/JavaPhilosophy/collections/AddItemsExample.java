7612c4a9c73c53c35c5d3419df0f464419bfc4b9
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-15 10:49:55 +0300

    update conspect, progress, tasks

--- Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/AddItemsExample.java
+++ Projects/JavaSandbox/src/main/java/JavaPhilosophy/collections/AddItemsExample.java
@@ -1,2 +1,29 @@
-package JavaPhilosophy.collections;public class AddItemsExample {
+package JavaPhilosophy.collections;
+
+import JavaSyntaxis.basics.Array;
+
+import java.util.*;
+
+public class AddItemsExample {
+    public static void main(String[] args) {
+        List<Integer> myLinkedList = new LinkedList<>(Arrays.<Integer>asList(2, 5, 1, 3, 4));
+        List<Integer> myArrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        Integer[] moreInts = {6, 7, 8, 9, 10};
+        myLinkedList.addAll(Arrays.asList(moreInts));
+        Collections.addAll(myLinkedList, 11, 12, 13, 14, 15, 16);
+        Collections.addAll(myLinkedList, moreInts);
+
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.set(1, 99);
+//        list.add(21); // Ошибка - нельзя изменить размер базового массива.
+
+
+        System.out.println(Arrays.toString(moreInts));
+        System.out.println(myLinkedList);
+        System.out.println(myArrayList);
+        System.out.println(list);
+
+    }
 }
