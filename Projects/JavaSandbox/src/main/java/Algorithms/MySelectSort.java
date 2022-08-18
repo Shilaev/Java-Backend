6331451df6995288400962041579015432905dd3
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-18 13:32:48 +0300

    update selectionSort

--- Projects/JavaSandbox/src/main/java/Algorithms/MySelectSort.java
+++ Projects/JavaSandbox/src/main/java/Algorithms/MySelectSort.java
@@ -1,2 +1,69 @@
-package Algorithms;public class MySelectSort {
+package Algorithms;
+
+import java.util.*;
+
+public class MySelectSort {
+
+    public static class SelectionSortingMethod {
+        static final String DESC = "DESC";
+        static final String ASC = "ASC";
+    }
+
+    public static List<Integer> selectSort(List<Integer> collection, String method) {
+        int size = collection.size();
+        List<Integer> newArray = new ArrayList<>(size);
+        int nextNumber = 0;
+
+        for (int i = 0; i < size; i++) {
+            switch (method) {
+                case SelectionSortingMethod.ASC -> nextNumber = findSmallest(collection);
+                case SelectionSortingMethod.DESC -> nextNumber = findBiggest(collection);
+                default -> throw new IllegalStateException("Unexpected value: " + method);
+            }
+            newArray.add(collection.remove(nextNumber));
+        }
+
+        return newArray;
+    }
+
+    private static Integer findBiggest(List<Integer> collection) {
+        Integer biggest = Integer.MIN_VALUE;
+
+        for (int j = 0; j < collection.size(); j++) {
+            Integer temp = collection.get(j);
+            if (biggest.compareTo(temp) < 0)
+                biggest = temp;
+        }
+
+        return collection.indexOf(biggest);
+    }
+
+    private static Integer findSmallest(List<Integer> collection) {
+        Integer smallest = Integer.MAX_VALUE;
+
+        for (int j = 0; j < collection.size(); j++) {
+            Integer temp = collection.get(j);
+            if (smallest.compareTo(temp) > 0)
+                smallest = temp;
+        }
+
+        return collection.indexOf(smallest);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> myNumbs = new ArrayList<>();
+        myNumbs.add(1);
+        myNumbs.add(4);
+        myNumbs.add(157);
+        myNumbs.add(190);
+        myNumbs.add(401);
+        myNumbs.add(300);
+        myNumbs.add(200);
+        myNumbs.add(240);
+        myNumbs.add(210);
+        myNumbs.add(3210);
+        myNumbs.add(0);
+
+        System.out.println(selectSort(myNumbs, SelectionSortingMethod.ASC));
+    }
 }
