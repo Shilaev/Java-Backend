8cb7bf0535049bea1fd7e62848403e714f2bf266
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-19 10:42:22 +0300

    update conspect and progress

--- Projects/JavaSandbox/src/main/java/Algorithms/RecursiveAlgorithms.java
+++ Projects/JavaSandbox/src/main/java/Algorithms/RecursiveAlgorithms.java
@@ -12,7 +12,7 @@ public class RecursiveAlgorithms {
     public static int factorial(int x) {
         if (x == 1) return 1;
         return factorial(x - 1) * x;
-    }   
+    }
 
     public static void main(String[] args) {
         // COUNTDOWN
