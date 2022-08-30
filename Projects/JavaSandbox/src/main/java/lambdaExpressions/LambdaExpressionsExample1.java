ea9b66fb02d62b717e40b3e7b682ad29303ded23
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-08-30 14:13:18 +0300

    update conspect

--- Projects/JavaSandbox/src/main/java/lambdaExpressions/LambdaExpressionsExample1.java
+++ Projects/JavaSandbox/src/main/java/lambdaExpressions/LambdaExpressionsExample1.java
@@ -1,2 +1,16 @@
-package lambdaExpressions;public class LambdaExpressionsExample1 {
+package lambdaExpressions;
+
+public class LambdaExpressionsExample1 {
+    public static void main(String[] args) {
+        Thread myThread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("do some");
+            }
+        });
+
+        Thread myThread2 = new Thread(() -> {
+            System.out.println("do some");
+        });
+    }
 }
