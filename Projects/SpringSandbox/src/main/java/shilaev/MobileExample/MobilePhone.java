6d11aa8bca48c6f57689ca5f66e1ebb4b1c0a048
shilaev.v <valentin.shilaev@gmail.com>
2022-09-16 17:42:16 +0300

    update spring creation object bean example

--- Projects/SpringSandbox/src/main/java/shilaev/MobileExample/MobilePhone.java
+++ Projects/SpringSandbox/src/main/java/shilaev/MobileExample/MobilePhone.java
@@ -1,2 +1,30 @@
-package shilaev.MobileExample;public class MobilePhone {
+package shilaev.MobileExample;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MobilePhone {
+    String modelTitle = "";
+
+    public MobilePhone(String modelTitle) {
+        this.modelTitle = modelTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "MobilePhone{" +
+                "modelTitle='" + modelTitle + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MobilePhone mobilePhone = context.getBean("mobilePhoneBean", MobilePhone.class);
+
+        System.out.println(mobilePhone);
+
+        context.close();
+    }
 }
