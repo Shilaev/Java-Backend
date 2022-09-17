76dd811a06a9c85e6f388e93e7bc37b9e17d6dfd
shilaev.v <valentin.shilaev@gmail.com>
2022-09-17 07:43:07 +0300

    update conspect

--- Projects/SpringSandbox/src/main/java/shilaev/MusicPlayerExample/MySpringSolution.java
+++ Projects/SpringSandbox/src/main/java/shilaev/MusicPlayerExample/MySpringSolution.java
@@ -2,16 +2,19 @@ package shilaev.MusicPlayerExample;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MySpring {
+public class MySpringSolution {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//
+//        MusicPlayer myMusicPlayer = new MusicPlayer(music);
+//        myMusicPlayer.play();
 
-        MusicPlayer myMusicPlayer = new MusicPlayer(music);
+        MusicPlayer myMusicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
         myMusicPlayer.play();
 
         context.close();
