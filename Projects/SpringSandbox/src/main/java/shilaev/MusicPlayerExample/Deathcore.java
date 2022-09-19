a873facfb5c45366bfd7035f92c7c4e421066f22
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-09-19 17:57:39 +0300

    update solution

--- Projects/SpringSandbox/src/main/java/shilaev/MusicPlayerExample/Deathcore.java
+++ Projects/SpringSandbox/src/main/java/shilaev/MusicPlayerExample/Deathcore.java
@@ -2,12 +2,18 @@ package shilaev.MusicPlayerExample;
 
 public class Deathcore implements Music {
 
+    private Deathcore() {}
+
+    public static Deathcore getDeathcoreMusic(){
+        return new Deathcore();
+    }
+
     @Override
     public String getSong() {
         return "AngelMaker - I long for rest";
     }
 
-    void doMyInit(){
+    void doMyInit() {
         System.out.println("INITIALIZATION");
     }
 
