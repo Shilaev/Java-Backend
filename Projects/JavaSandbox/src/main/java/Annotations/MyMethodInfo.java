048bab37d3ab257196360491cf7c1fc41adc8081
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-09-20 19:12:54 +0300

    update conspect

--- Projects/JavaSandbox/src/main/java/Annotations/MyMethodInfo.java
+++ Projects/JavaSandbox/src/main/java/Annotations/MyMethodInfo.java
@@ -1,14 +1,34 @@
 package Annotations;
 
-public @interface MyAuthor {
-    String name() default "Unnamed";
-    int dateOfBirth() default 0;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyMethodInfo {
+    String author() default "NoAuthor";
+
+    int dateOfCreation() default  2022;
+
+    String purpose() default "no description";
 }
 
-class TestAnnotation{
+class TestAnnotation {
 
-    @MyAuthor(name = "name", dateOfBirth = 12)
+    @MyMethodInfo(author = "Author", dateOfCreation = 2022, purpose = "sout Hello")
+    public void printHello() {
+        System.out.println("Hello");
+    }
+
+    @MyMethodInfo
+    public void printHello2(){
+        System.out.println("Hello2");
+    }
+
     public static void main(String[] args) {
-
+        TestAnnotation myTestAnnotation = new TestAnnotation();
+        myTestAnnotation.printHello();
     }
 }
