898d440ca61e20057cfc84aa55786d7e77bc715f
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-10-07 09:28:13 +0300

    update project

--- Projects/TestJavaEEProject/src/main/java/ru/shilaev/testjavaeeproject/MyTestServlet.java
+++ Projects/TestJavaEEProject/src/main/java/ru/shilaev/testjavaeeproject/MyTestServlet.java
@@ -1,12 +1,17 @@
+package ru.shilaev.testjavaeeproject;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("<h1>HELLO</h1>");
+        printWriter.println("</html>");
     }
 
     @Override
