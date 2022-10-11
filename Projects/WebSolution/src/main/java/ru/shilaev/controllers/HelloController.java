c6e4af2d16966cc574d6ec7fd313c8eba50cd4f8
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-10-11 15:46:56 +0300

    update project

--- Projects/WebSolution/src/main/java/ru/shilaev/controllers/HelloController.java
+++ Projects/WebSolution/src/main/java/ru/shilaev/controllers/HelloController.java
@@ -1,12 +1,25 @@
-package ru.shilaev;
+package ru.shilaev.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/greetings")
 public class HelloController {
+
     @GetMapping("/hello")
-    public String sayHello() {
-        return "hello";
+    public String hello(@RequestParam(value = "name", required = false) String name,
+                        Model model) {
+        model.addAttribute("message", "Hello, " + name + "!");
+        return "hello/hello";
     }
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello/hello_world";
+    }
+
 }
--- Projects/WebSolution/src/main/java/ru/shilaev/controllers/calculatorController.java
+++ Projects/WebSolution/src/main/java/ru/shilaev/controllers/calculatorController.java
@@ -1,2 +1,41 @@
-package ru.shilaev.controllers;public class calculatorController {
+package ru.shilaev.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/first")
+public class calculatorController {
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("number1") Integer number1,
+                             @RequestParam("number2") Integer number2,
+                             @RequestParam("operation") String operation,
+                             Model model) {
+
+        double result;
+        switch (operation) {
+            case "multiply":
+                result = number1 + number2;
+                break;
+            case "subtract":
+                result = number1 / number2;
+                break;
+            case "plus":
+                result = number1 + number2;
+                break;
+            case "minus":
+                result = number1 - number2;
+                break;
+            default:
+                result = 0;
+        }
+        model.addAttribute("result", result);
+
+        return "first/result";
+
+    }
 }
