2986c7daad7c69955272e5e6f81041c52e313e35
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-11-03 01:51:17 +0300

    add Graph and BFS algorytm

---
+++ Projects/JavaSandbox/src/main/java/Algorithms/Graph/Graph.java
@@ -0,0 +1,18 @@
+package Algorithms.Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyGraph {
+    
+    public MyGraph(int elem) {
+
+    }
+}
+
+class TestGraph {
+    public static void main(String[] args) {
+        MyGraph myGraph;
+    }
+}
