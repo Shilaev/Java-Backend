754107ec4ffbb6201df110b40182bf4e435b1c40
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-11-11 01:13:44 +0300

    update

--- Projects/SpringXMLConfiguration/src/main/java/shilaev/Solution.java
+++ Projects/SpringXMLConfiguration/src/main/java/shilaev/Solution.java
@@ -1,20 +1,25 @@
 package shilaev;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import shilaev.messageMaker.MessageMaker;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class Solution {
+//    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml"
+//        );
+//
+//        MessageMaker helloMessage = context.getBean("messageMaker.helloPrinter", MessageMaker.class);
+//        helloMessage.print();
+//
+//        MessageMaker byeMessage = context.getBean("messageMaker.byePrinter", MessageMaker.class);
+//        byeMessage.print();
+//    }
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
-
-        MessageMaker helloMessage = context.getBean("messageMaker.helloPrinter", MessageMaker.class);
-        helloMessage.print();
-
-        MessageMaker byeMessage = context.getBean("messageMaker.byePrinter", MessageMaker.class);
-        byeMessage.print();
-
+    @RequestMapping(value = "/hello")
+    public String getHello() {
+        return "hello";
     }
 }
