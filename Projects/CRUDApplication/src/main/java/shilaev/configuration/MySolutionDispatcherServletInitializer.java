35136742589e53729f907477fde4d10cb7127b31
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-11-18 12:18:20 +0300

    update conspect and CRUDapp

---
+++ Projects/CRUDApplication/src/main/java/shilaev/configuration/MySolutionDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package shilaev.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySolutionDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
