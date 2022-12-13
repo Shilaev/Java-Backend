3c6c918f4133f94f8588bfeba44ba1b5a2ac33a6
shilaev.v <valentin.shilaev@gmail.com>
2022-12-13 18:17:58 +0300

    updapte

--- Projects/JavaSandbox/src/main/java/lambdaExpressions/LambdaMoreExample.java
+++ Projects/JavaSandbox/src/main/java/lambdaExpressions/LambdaMoreExample.java
@@ -1,2 +1,16 @@
-package lambdaExpressions;public class LambdaMoreExample {
+package lambdaExpressions;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class LambdaMoreExample {
+    public static void main(String[] args) {
+
+        Supplier<Double> supplier = () -> new Random().nextDouble();
+        Double doubleOptional = Optional.of(4.5).orElseGet(supplier);
+
+        System.out.println(doubleOptional);
+
+    }
 }
