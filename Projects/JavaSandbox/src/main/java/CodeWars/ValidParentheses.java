e143bcf8a5d89c9e819a0d5a67274ffb1e7d6afa
shilaev.v <valentin.shilaev@gmail.com>
2022-12-20 20:50:43 +0300

    + leetCode Valid Parentheses solution

--- Projects/JavaSandbox/src/main/java/CodeWars/ValidParentheses.java
+++ Projects/JavaSandbox/src/main/java/CodeWars/ValidParentheses.java
@@ -1,22 +1,33 @@
-package Ariphmmetica;
+package CodeWars;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
-public class Solution {
+public class ValidParentheses {
     public static void main(String[] args) {
         System.out.println(checkString(""));
     }
 
-    public static boolean checkString(String s) {
-        String[] splited = s.split("");
-        boolean isCorrect = false;
+    public static boolean checkString(String parens) {
+        char[] lineForCheck = parens.toCharArray();
+        List<Character> brackets = new ArrayList<>(List.of('(', ')', '{', '}', '[', ']'));
+        Stack<Character> charList = new Stack<>();
 
-        Map<String, String> bracketsConformity = new HashMap<>();
-        bracketsConformity.put("(", ")");
-        bracketsConformity.put("[", "]");
-        bracketsConformity.put("{", "}");
-
-        return isCorrect;
+        for (char currentChar : lineForCheck) {
+            if (!brackets.contains(currentChar)) continue;
+            
+            if (currentChar == '(') charList.add('(');
+            else if (currentChar == '{') charList.add('{');
+            else if (currentChar == '[') charList.add('[');
+            else if (currentChar == ')' && !charList.isEmpty() && charList.contains('(') && charList.peek() == '(')
+                charList.pop();
+            else if (currentChar == '}' && !charList.isEmpty() && charList.contains('{') && charList.peek() == '{')
+                charList.pop();
+            else if (currentChar == ']' && !charList.isEmpty() && charList.contains('[') && charList.peek() == '[')
+                charList.pop();
+            else return false;
+        }
+        return charList.isEmpty();
     }
 }
