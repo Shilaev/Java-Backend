011bd3c10c66e9b3d1db56d393c538068d3932d0
shilaev.v <valentin.shilaev@gmail.com>
2022-12-20 23:34:09 +0300

    + CircleCipher

--- Projects/JavaSandbox/src/main/java/CodeWars/CircleCipher.java
+++ Projects/JavaSandbox/src/main/java/CodeWars/CircleCipher.java
@@ -1,2 +1,25 @@
-package CodeWars;public class CircleCipher {
+package CodeWars;
+
+public class CircleCipher {
+    public static void main(String[] args) {
+        System.out.println(encode("qwert"));
+        System.out.println(decode(encode("qwert")));
+    }
+
+    public static String encode(String s) {
+        StringBuilder resultString = new StringBuilder();
+        for (int i = 0, j = s.length(); j > i; i++) {
+            resultString.append(s.charAt(i));
+            if (--j > i) resultString.append(s.charAt(j));
+        }
+        return resultString.toString();
+    }
+
+    public static String decode(String s) {
+        StringBuilder resultStringL = new StringBuilder();
+        StringBuilder resultStringR = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+            (i % 2 == 0 ? resultStringL : resultStringR).append(s.charAt(i));
+        return resultStringL.append(resultStringR.reverse()).toString();
+    }
 }
