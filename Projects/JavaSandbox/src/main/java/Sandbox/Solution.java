351686fa370d6b46d50f55a1d7a1aa5a6f454664
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-12-21 00:09:42 +0300

    update

--- Projects/JavaSandbox/src/main/java/Sandbox/Solution.java
+++ Projects/JavaSandbox/src/main/java/Sandbox/Solution.java
@@ -1,2 +1,37 @@
-package Sandbox;public class Solution {
+package Sandbox;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+
+    public static boolean isValid(String s) {
+        String[] characters = s.split("");
+
+        Map<String, String> brackets = new HashMap<>();
+        brackets.put("(", ")");
+        brackets.put("[", "]");
+        brackets.put("{", "}");
+
+        for (int i = 0; i < characters.length; i++) {
+            String current = characters[i];
+            String next = characters[i + 1];
+
+            if (!brackets.get(current).equals(next)) {
+                return false;
+            }
+
+            i++;
+        }
+
+        return true;
+    }
 }
+
+
--- Projects/JavaSandbox/src/main/java/YandexContest/NameAndColor.java
+++ Projects/JavaSandbox/src/main/java/YandexContest/NameAndColor.java
@@ -1,2 +1,32 @@
-package YandexContest;public class NameAndColor {
+package YandexContest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NameAndColor {
+    public static void main(String[] args) {
+
+        // N
+        // Algorithms and Data Structures
+        // BBBBBBBBBB BYB YYYY BBBBBBBBBB
+//
+//        int length = new Scanner(System.in).nextInt();
+//        String[] names = new Scanner(System.in).nextLine().split(" ");
+//        char[] color = new Scanner(System.in).nextLine().toCharArray();
+
+//        List<String> strings = new ArrayList<String>();
+//        int index = 0;
+//        while (index < text.length()) {
+//            strings.add(text.substring(index, Math.min(index + 4,text.length())));
+//            index += 4;
+//        }
+
+        int length = 27;
+        String[] names = "Algorithms and Data Structures".split(" ");
+        String color = "BBBBBBBBBBBYBYYYYBBBBBBBBBB";
+        String[] colors = new String[length];
+
+
+    }
 }
