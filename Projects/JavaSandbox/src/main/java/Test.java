1f40938881a41545e356713ce8b53956a0973aeb
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-12-30 12:11:06 +0300

    upd

--- Projects/JavaSandbox/src/main/java/Test.java
+++ Projects/JavaSandbox/src/main/java/Test.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        String regexp = "^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$";
-
-        String isbn = "978-2-1234-5680-3";
-        System.out.println(isbn.matches(regexp));
+        
     }
 }
