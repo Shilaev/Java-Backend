9375440f4161eff75a70b75e3fd0d947ade9f3a6
Shilaev Valentin <valentin.shilaev@yandex.ru>
2023-01-06 13:45:47 +0300

    update

--- Projects/JavaSandbox/src/main/java/Algorithms/addComma.java
+++ Projects/JavaSandbox/src/main/java/Algorithms/addComma.java
@@ -1,2 +1,16 @@
-package Algorithms;public class addComma {
+package Algorithms;
+
+public class addComma {
+    public static StringBuffer add(StringBuffer str) {
+        int len = str.length() - 1;
+        if (len < 3) {
+            return str;
+        }
+
+        StringBuffer newStr = new StringBuffer(str);
+        newStr.delete(len - 3, len);
+        return add(newStr)
+                .append(".")
+                .append(str, len - 3, len);
+    }
 }
--- Projects/JavaSandbox/src/main/java/Test.java
+++ Projects/JavaSandbox/src/main/java/Test.java
@@ -1,9 +1,13 @@
-import java.util.ArrayList;
+import Algorithms.addComma;
+
 import java.util.Arrays;
-import java.util.List;
+import java.util.function.IntUnaryOperator;
 
 public class Test {
+
     public static void main(String[] args) {
 
     }
+
+
 }
