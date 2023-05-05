cf99077a37049078692cd5b378bc950b5ca6173e
shilaevvalentin <valentin.shilaev@yandex.ru>
2023-05-05 15:19:19 +0300

    change pom file

--- Projects/JavaBootApplication/src/main/java/ru/shilaev/javabootapplication/JavaBootApplication.java
+++ Projects/JavaBootApplication/src/main/java/ru/shilaev/javabootapplication/JavaBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaBootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBootApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBootApplication.class, args);
+    }
 }
