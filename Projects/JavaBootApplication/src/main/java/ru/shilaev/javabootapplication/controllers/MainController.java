50e3c1a81dcbe05738b1d73b929e3af0b46e074b
shilaevvalentin <valentin.shilaev@yandex.ru>
2023-05-05 16:06:54 +0300

    changes

--- Projects/JavaBootApplication/src/main/java/ru/shilaev/javabootapplication/controllers/MainController.java
+++ Projects/JavaBootApplication/src/main/java/ru/shilaev/javabootapplication/controllers/MainController.java
@@ -1,2 +1,19 @@
-package ru.shilaev.javabootapplication.controllers;public class MainController {
+package ru.shilaev.javabootapplication.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/hello")
+    public String getHello() {
+        System.out.println(hello);
+        return "hello";
+    }
 }
